cc823a6a682d8a21d3acf22bdefe5f79eceb9238
Silas Nordgren <nordgrensilas@gmail.com>
2019-07-29 21:33:26 +0200

    Add more functionality

--- README.md
+++ README.md
@@ -3,12 +3,20 @@
 A project for generating art assets from JavaScript code using the Canvas API 
 in the browser. My use case is making simple assets for mobile games. 
 
+    fillStyle ('red')
+    rect (50, 50, 20, 20)
+    fill ()
+
 ## Image dimensions
-The default dimensions of the generated canvas is 512x512. 
+The default dimensions of the generated canvas is 512x512. Origin is at the 
+bottom left corner.
 
 ## Saving your image file
 To save your image, right click on the image and select "Save image as...". 
 There's no better way to download your image file yet. 
 
 ## Why wouldn't I just script the Canvas API?
-This tool has live preview and makes drawing less cluttered.
+This tool has live preview and eliminates clutter in the image code.  
+
+## Screenshots
+![Circle with a rectangle](screenshots/circle-with-rect.png)
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function updateCanvas() {
   const sourceCodeInput = document.getElementById('source-code-input');
   const sourceCode = sourceCodeInput.value;
-  const ctx = retrieveDrawContext(512);
-  evalSource(ctx, sourceCode);
+  const canvasSize = 512;
+  const ctx = retrieveDrawContext(canvasSize);
+  evalSource(ctx, canvasSize, sourceCode);
 }
 
 function retrieveDrawContext(canvasSize) {
@@ -14,12 +15,40 @@ function retrieveDrawContext(canvasSize) {
   return ctx;
 }
 
-function evalSource(ctx, src) {
-  const rect = (x, y, w, h) => ctx.rect(x, y, w, h);
+function evalSource(ctx, canvasSize, src) {
+
+  const beginPath = () => {
+    ctx.beginPath();
+  }
+  
+  const circle = (x, y, radius) => {
+    ctx.arc(x + radius / 2, canvasSize - y - radius / 2, radius / 2, radius / 2, 0, 2 * Math.PI);
+  }
+
   const fill = () => ctx.fill();
-  const stroke = () => ctx.stroke();
+
   const fillStyle = (style) => {
     ctx.fillStyle = style;
+  };
+
+  const lineTo = (x, y) => {
+    ctx.lineTo(x, canvasSize - y - 1);
+  }
+  
+  const moveTo = (x, y) => {
+    ctx.moveTo(x, canvasSize - y - 1);
+  };
+
+  const pi = Math.PI;
+
+  const rect = (x, y, w, h) => {
+    ctx.rect(x, canvasSize - y - h, w, h);
+  };
+
+  const stroke = () => ctx.stroke();
+
+  const strokeStyle = (style) => {
+    ctx.strokeStyle = style;
   };
 
   const result = eval(src);
