88f9233a6b6f2f4dedf6d830df02327593ef8535
Silas Nordgren <nordgrensilas@gmail.com>
2019-07-29 21:05:57 +0200

    Use JavaScript for interpretation

---
+++ README.md
@@ -0,0 +1,14 @@
+# art with code
+
+A project for generating art assets from JavaScript code using the Canvas API 
+in the browser. My use case is making simple assets for mobile games. 
+
+## Image dimensions
+The default dimensions of the generated canvas is 512x512. 
+
+## Saving your image file
+To save your image, right click on the image and select "Save image as...". 
+There's no better way to download your image file yet. 
+
+## Why wouldn't I just script the Canvas API?
+This tool has live preview and makes drawing less cluttered.
--- index.js
+++ index.js
@@ -1,50 +1,8 @@
 function updateCanvas() {
   const sourceCodeInput = document.getElementById('source-code-input');
   const sourceCode = sourceCodeInput.value;
-  const normalizedSourceCode = normalizeSourceCode(sourceCode);
-  interpret(normalizedSourceCode);
-}
-
-function normalizeSourceCode(sourceCode) {
-  const trimmed = sourceCode.trim().replace('\t', ' ').replace('\r', ' ');
-  let remaining = trimmed;
-  while (remaining.includes('  ')) {
-    remaining = remaining.replace('  ', ' ');
-  }
-  return remaining;
-}
-
-function interpret(sourceCode) {
-  const blocks = sourceCode.split('\n\n');
-  const procedures = findProcedures(blocks);
-
-  const canvasSize = 512;
-  const ctx = retrieveDrawContext(canvasSize);
-
-  for (const block of blocks) {
-    if (!block.startsWith(':')) {
-      ctx.fillStyle = 'black';
-
-      const lines = block.split('\n');
-      for (const line of lines) {
-        const parts = line.split(' ');
-        if (parts.length > 0 && parts[0].length > 0) {
-          if (parts[0] === 'rect' && parts.length === 5) {
-            const transform = n => (n / 100 * canvasSize) | 0;
-            const par = index => transform(parseInt(parts[index]));
-            const x = par(1);
-            const y = par(2);
-            const w = par(3);
-            const h = par(4);
-            
-            ctx.fillRect((x - w / 2) | 0, (canvasSize - y - h / 2) | 0, w, h);
-          } else if (parts[0] === 'color' && parts.length === 2) {
-            ctx.fillStyle = parts[1];
-          }
-        }
-      }
-    }
-  }
+  const ctx = retrieveDrawContext(512);
+  evalSource(ctx, sourceCode);
 }
 
 function retrieveDrawContext(canvasSize) {
@@ -56,33 +14,13 @@ function retrieveDrawContext(canvasSize) {
   return ctx;
 }
 
-function findProcedures(blocks) {
-  const procedures = {};
+function evalSource(ctx, src) {
+  const rect = (x, y, w, h) => ctx.rect(x, y, w, h);
+  const fill = () => ctx.fill();
+  const stroke = () => ctx.stroke();
+  const fillStyle = (style) => {
+    ctx.fillStyle = style;
+  };
 
-  for (const block of blocks) {
-    if (block.startsWith(':')) {
-      const lines = block.split('\n');
-      if (lines.length > 0) {
-        const firstLine = lines[0].trim();
-        const parts = firstLine.split(' ');
-        const name = parts[0].substring(1);
-
-        const parameters = [];
-        for (let i = 1; i < parts.length; i++) {
-          parameters.push(parts[i]);
-        }
-
-        const operations = [];
-        for (let i = 1; i < lines.length; i++) {
-          operations.push(lines[i]);
-        }
-
-        procedures[name] = {
-          name,
-          parameters,
-          operations
-        };
-      }
-    }
-  }
+  const result = eval(src);
 }
